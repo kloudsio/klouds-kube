5826927f4dbea5c9fed82b8c3e2474d733429f71
Daniel Sont <dan.sont@gmail.com>
2015-09-23 01:35:32 +0700

    TCP heartbeat. Now gathers: env vars, hostname, memory, network, and koa data

--- deploy-foo/app.js
+++ deploy-foo/app.js
@@ -7,35 +7,43 @@ import os from 'os'
 import net from 'net'
 
 
+let fieldreport = {
+	hertz: "bmmbmmpm bmmbmmpm... bmmbmmpm bmmbmmpm...",
+	hostname: os.hostname(),
+	memory: {
+		free: os.freemem() / 1024 / 1024 + " i.e. (" + os.freemem() + ")",
+		total: os.totalmem() / 1024 / 1024 + " i.e. (" + os.totalmem() + ")",
+	},
+	network: os.networkInterfaces(),
+	env: process.env
+}
+
 let app = koa()
 app.use(json({pretty: true}))
 
 app.use(function*(next) {
-	yield next;
-	this.body = {
-		hertz: "bmmbmmpm bmmbmmpm... bmmbmmpm bmmbmmpm...",
-		hostname: os.hostname(),
-		memory: {
-			free: os.freemem() / 1024 / 1024 + " i.e. (" + os.freemem() + ")",
-			total: os.totalmem() / 1024 / 1024 + " i.e. (" + os.totalmem() + ")",
-		},
-		network: os.networkInterfaces(),
-		ctx: this
-	}
+	yield next
+	this.body = fieldreport
+	this.body.koa = this
+	this.body.env = process.env
 })
 
 console.log("Heart goes bmmbmmpm bmmbmmpm??\n")
 console.log("\ncurl -i localhost:8080/heartbeat\n\n")
 
-http.createServer(app.callback()).listen(1337, () => console.log(` bmmbmpppm?  -> curl -i :1337/heartbeat`))
-http.createServer(app.callback()).listen(8000, () => console.log(` bmmbmpppm?  -> curl -i :8000/heartbeat`))
-http.createServer(app.callback()).listen(8080, () => console.log(` bmmbmpppm?  -> curl -i :8080/heartbeat`))
-http.createServer(app.callback()).listen(8081, () => console.log(` bmmbmpppm?  -> curl -i :8081/heartbeat`))
 
+http.createServer(app.callback()).listen(8000, () => console.log(` bmmbmpppm?  -> curl -i localhost:8000/heartbeat`))
+http.createServer(app.callback()).listen(8080, () => console.log(` bmmbmpppm?  -> curl -i localhost:8080/heartbeat`))
 
-try	{
-	// .listen(80, () => console.log("success: 80"))
-} catch (e) {
-	// console.error("port 80 died...")
-	// console.error(e)
-}
+const server = net.createServer(c => {
+    console.log('client connected');
+    c.on('end', () => {
+        console.log('client disconnected');
+    });
+    c.write('Hello. \r\n');
+    c.write(JSON.stringify(c.socket));
+    c.pipe(c);
+});
+server.listen(8124, () => {
+    console.log('TCP Server bound 8124  -> telnet localhost 8124');
+});
